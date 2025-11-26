1632750f6bdffdcccd6e6c8be8dbb85e1d7167ba
Daria Kulikova <dariakulikova01@gmail.com>
2025-11-26 16:33:15 +0300

    add templates/index.html, modified app.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-    return jsonify("Welcome to Page Analyzer!")
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
