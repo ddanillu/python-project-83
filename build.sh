61ec1da0248356255d96b02d44bff6750cb9394e
Daria Kulikova <dariakulikova01@gmail.com>
2025-12-05 13:24:59 +0300

    modified build.sh: update path to db

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 # дальше подключаемся к база данных
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
-make install && psql -a -d $DATABASE_URL -f database.sql
+make install && psql -a -d $DATABASE_URL -f db/database.sql
